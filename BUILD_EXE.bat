26ba22aef8ba23527d0549f6a23dc44d8b06c3ed
mitsubishievo99 <florian.weissensteiner@gmail.com>
2023-07-16 17:47:25 +0200

    template matching

--- BUILD_EXE.bat
+++ BUILD_EXE.bat
@@ -1,3 +1,3 @@
 call env\Scripts\activate.bat
-pyinstaller -F --distpath exe --upx-dir=test\upx-4.0.2-win64 -n fullscreen main.py
+pyinstaller -F --distpath exe --upx-dir=test\upx-4.0.2-win64 -n fullscreen main2.py
 pause
--- fullscreen.spec
+++ fullscreen.spec
@@ -5,7 +5,7 @@ block_cipher = None
 
 
 a = Analysis(
-    ['main.py'],
+    ['main2.py'],
     pathex=[],
     binaries=[],
     datas=[],
